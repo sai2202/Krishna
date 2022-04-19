beb175ef9a9ab400d8a0a46f77e68f1cff2160db
venkata sai krishna <60344507+sai2202@users.noreply.github.com>
2022-04-19 13:28:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,36 @@ provider "azurerm" {
   features {
   }
 }
+
+###################
+
+locals {
+
+  action_group = jsondecode(file("${path.module}/actiongroup.json"))
+  resource = {
+  for list in local.action_group : list.type => list
+  }
+}
+############################
+##########  Creating ResourceGroup   #######################
+resource "azurerm_resource_group" "rg" {
+  name     = "test"
+  location = "eastus
+}
+##################################
+
+resource "azurerm_monitor_action_group" "monitor_action_group" {
+  for_each            = local.resource
+  name                = "test-${each.value.type}"
+  resource_group_name = azurerm_resource_group.rg.name
+  short_name          = "short-alert"
+
+  dynamic "email_receiver" {
+    for_each = each.value.type
+    content {
+      name                    = "Email"
+      email_address           = email_receiver.value.primary_support # here it is not working 
+      use_common_alert_schema = true #Enables or disables the common alert schema
+    }
+  }
+}
